6945e90fa112a60c7e8a3deba924dddf41150b4c
Rafael <rafaelaugustodev@gmail.com>
2023-12-17 18:50:26 -0300

    Criando os repositories

---
+++ src/main/java/com/todolist/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.todolist.repositories;
+
+import com.todolist.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ src/main/java/com/todolist/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.todolist.repositories;
+
+import com.todolist.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
