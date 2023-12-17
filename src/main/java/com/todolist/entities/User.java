1d5fe7c938a9bbb8b3dc252470744d78e146ae19
Rafael <rafaelaugustodev@gmail.com>
2023-12-17 18:49:22 -0300

    Adição do campo email

--- src/main/java/com/todolist/entities/User.java
+++ src/main/java/com/todolist/entities/User.java
@@ -27,6 +27,9 @@ public class User{
     @Column (nullable = false)
     private String password;
 
+    @Column (nullable = false)
+    private String email;
+
     @CreationTimestamp
     private LocalDateTime createdAt;
 
