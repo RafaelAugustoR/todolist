012d58249fa95afd35bb0c5c49ad84a619e01814
Rafael <rafaelaugustodev@gmail.com>
2023-12-17 21:03:43 -0300

    Atualizando método save

--- src/main/java/com/todolist/services/UserService.java
+++ src/main/java/com/todolist/services/UserService.java
@@ -3,6 +3,7 @@ package com.todolist.services;
 
 import com.todolist.dtos.UserDTO;
 import com.todolist.repositories.UserRepository;
+import com.todolist.validators.UserValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,10 @@ public class UserService {
     private UserRepository userRepository;
 
     public UserDTO save(UserDTO user) {
+        List<String> errors = UserValidator.validateUser(user);
+        if (!errors.isEmpty()) {
+            log.error("User is not valid {}", user);
+        }
         return UserDTO.fromEntity(userRepository.save(UserDTO.toEntity(user)));
     }
 
