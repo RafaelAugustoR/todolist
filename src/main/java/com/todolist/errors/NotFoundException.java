953f6224129cd5ff8a49159e870f59db17bf5335
Rafael <rafaelaugustodev@gmail.com>
2023-12-27 19:05:20 -0300

    Tratamento de exceções

--- src/main/java/com/todolist/errors/NotFoundException.java
+++ src/main/java/com/todolist/errors/NotFoundException.java
@@ -3,6 +3,18 @@ package com.todolist.errors;
 public class NotFoundException extends RuntimeException {
 
     private ErrorCodes errorCode;
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFoundException(String message, Throwable cause, ErrorCodes errorCode) {
+        super(message, cause);
+        this.errorCode = errorCode;
+    }
 
     public NotFoundException(String message, ErrorCodes errorCode) {
         super(message);
