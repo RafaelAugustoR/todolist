f3f12c408962602de87fef31ad233a5fd352ad19
Rafael <rafaelaugustodev@gmail.com>
2023-12-27 19:11:59 -0300

    Adição de exceções para category e tasks

--- src/main/java/com/todolist/errors/ErrorCodes.java
+++ src/main/java/com/todolist/errors/ErrorCodes.java
@@ -3,7 +3,11 @@ package com.todolist.errors;
 
 public enum ErrorCodes {
     USER_NOT_FOUND(1000),
+    CATEGORY_NOT_FOUND(1001),
+    TASK_NOT_FOUND(1002),
     USER_NOT_VALID(2000),
+    CATEGORY_NOT_VALID(2001),
+    TASK_NOT_VALID(2002),
     ;
 
     private int code;
