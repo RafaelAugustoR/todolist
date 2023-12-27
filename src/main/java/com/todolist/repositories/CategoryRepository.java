9558eb236eb949d6869ec11cff2c8546c8a4bbd2
Rafael <rafaelaugustodev@gmail.com>
2023-12-27 19:09:25 -0300

    Implementação de consulta personalizada

--- src/main/java/com/todolist/repositories/CategoryRepository.java
+++ src/main/java/com/todolist/repositories/CategoryRepository.java
@@ -3,9 +3,14 @@ package com.todolist.repositories;
 import com.todolist.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    List<Category> findCategoryByUserId(Long userId);
+
+    @Query("select t.category.id from tb_tasks t where t.id = :taskId")
+    Long findCategoryByTaskId(@Param("taskId") Long taskId);
 }
